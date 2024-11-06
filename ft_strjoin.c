04572c024172bf7cfbe04322695a1572e5bef4cf
Ayoub Nachat <anachat@e3r4p5.1337.ma>
2024-11-06 10:54:20 +0100

    add some updates

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: anachat <anachat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:39:20 by anachat           #+#    #+#             */
-/*   Updated: 2024/10/27 16:55:48 by anachat          ###   ########.fr       */
+/*   Updated: 2024/11/06 10:27:10 by anachat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
+	if (!s1 || !s2)
+		return (NULL);
 	str = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (s1[i])
-	{
 		str[j++] = s1[i++];
-	}
 	i = 0;
 	while (s2[i])
-	{
 		str[j++] = s2[i++];
-	}
 	str[j] = '\0';
 	return (str);
 }
